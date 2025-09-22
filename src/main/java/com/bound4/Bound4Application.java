bad95aaeb38cbd39d625d11d55e7a6f272a43b5f
sangminna <sm9171@nate.com>
2025-09-22 21:41:56 +0900

    init

---
+++ src/main/java/com/bound4/Bound4Application.java
@@ -0,0 +1,13 @@
+package com.bound4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bound4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bound4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bound4/Bound4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bound4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bound4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
