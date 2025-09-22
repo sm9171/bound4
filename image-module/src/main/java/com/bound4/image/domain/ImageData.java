0f433b07530e0ea2451bd96dfd1d657f43575182
sangminna <sm9171@nate.com>
2025-09-23 00:09:45 +0900

    refactor: ImageData 클래스 수정 - null 안전성 강화: of 메서드에서 사전 검증 - 배열 equality 문제 해결: Arrays.equals() 및 Arrays.hashCode() 구현 - 불변성 보장: 데이터 클론을 통한 참조 격리

--- image-module/src/main/java/com/bound4/image/domain/ImageData.java
+++ image-module/src/main/java/com/bound4/image/domain/ImageData.java
@@ -1,5 +1,7 @@
 package com.bound4.image.domain;
 
+import java.util.Arrays;
+
 public record ImageData(byte[] data) {
     public ImageData {
         if (data == null || data.length == 0) {
@@ -8,6 +10,9 @@ public record ImageData(byte[] data) {
     }
     
     public static ImageData of(byte[] data) {
+        if (data == null || data.length == 0) {
+            throw new IllegalArgumentException("Image data cannot be null or empty");
+        }
         return new ImageData(data.clone());
     }
     
@@ -17,5 +22,18 @@ public record ImageData(byte[] data) {
     
     public long getSize() {
         return data.length;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ImageData imageData = (ImageData) obj;
+        return Arrays.equals(data, imageData.data);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(data);
     }
 }
