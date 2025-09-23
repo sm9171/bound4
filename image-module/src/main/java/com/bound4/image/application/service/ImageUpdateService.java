40f2f5fd7cbb6e3a607c8bef40e76534296176b6
sangminna <sm9171@nate.com>
2025-09-23 22:29:48 +0900

    feat: 이미지 수정 Service 구현
    - 낙관적 락 기반 이미지 수정 비즈니스 로직 구현
    - 버전 불일치 검증 및 예외 처리
    - 부분 업데이트 지원으로 null 값 필드는 기존 값 유지

---
+++ image-module/src/main/java/com/bound4/image/application/service/ImageUpdateService.java
@@ -0,0 +1,40 @@
+package com.bound4.image.application.service;
+
+import com.bound4.image.adapter.in.web.exception.ImageNotFoundException;
+import com.bound4.image.application.port.in.ImageUpdateCommand;
+import com.bound4.image.application.port.in.ImageUpdateUseCase;
+import com.bound4.image.application.port.out.ImageRepository;
+import com.bound4.image.domain.Image;
+import com.bound4.image.domain.ImageId;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ImageUpdateService implements ImageUpdateUseCase {
+    
+    private final ImageRepository imageRepository;
+    
+    public ImageUpdateService(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+    
+    @Override
+    public Image updateImage(ImageUpdateCommand command) {
+        ImageId imageId = ImageId.of(command.getImageId());
+        
+        Image image = imageRepository.findById(imageId)
+                .orElseThrow(() -> new ImageNotFoundException(command.getImageId()));
+        
+        if (!command.getVersion().equals(image.getVersion())) {
+            throw new OptimisticLockingFailureException(
+                "Image has been modified by another process. Expected version: " + 
+                command.getVersion() + ", but was: " + image.getVersion());
+        }
+        
+        image.updatePartially(command.getTags(), command.getMemo(), command.getStatus());
+        
+        return imageRepository.updateWithOptimisticLock(image);
+    }
+}
